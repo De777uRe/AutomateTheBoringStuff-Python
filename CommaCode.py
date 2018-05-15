360262e3241e58457f97db1ad0b9db5f1602e223
William Goins <de777urest@gmail.com>
2018-05-14 20:37:04 -0500

    Initial Completion

---
+++ CommaCode.py
@@ -0,0 +1,13 @@
+def list_to_str(some_list):
+    list_str = ''
+    for i in range(len(some_list) - 1):
+        list_str += str(some_list[i])
+        list_str += ", "
+    list_str += "and " + str(some_list[-1])
+    return list_str
+
+
+my_list = [1, 2, 3, 4, 5]
+print(list_to_str(my_list))
+my_test_list = ['apples', 'bananas', 'tofu', 'cats']
+print(list_to_str(my_test_list))
